80381f7af89c1d95cc08e970055750ce1aaa07c0
BlackHat <123658198+Mubaraq0@users.noreply.github.com>
2024-06-23 08:16:21 +0100

    Update Trendinggoogle.ts

--- src/Commands/Utils/Trendinggoogle.ts
+++ src/Commands/Utils/Trendinggoogle.ts
@@ -3,18 +3,18 @@ import { IArgs } from '../../Types'
 import request from '../../lib/request'
 import axios from 'axios'
 
-@Command('help', {
+@Command('trendginggoogle', {
     description: "Displays the bot's usable commands",
-    aliases: ['h', 'menu', 'commands'],
-    cooldown: 5,
-    exp: 20,
-    usage: 'help || help <command_name>',
-    category: 'general'
+    aliases: ['googletrenging', 'tgoogle', 'tg'],
+    cooldown: 3,
+    exp: 5,
+    usage: 'trendinggoogle',
+    category: 'utils'
 })
 
   export default class extends BaseCommand {
     public override execute = async (M: Message, { context }: IArgs): Promise<void> => {
-          if (!joined) return void M.reply('✖ Provide an item name to search, Baka!')
+          if (!context) return void M.reply('✖ Provide an item name to search, Baka!')
         const chitoge = joined.trim()
         console.log(chitoge)
         const { data } = await axios.get(`https://api-xcoders.xyz/api/info/trend/google?country=${chitoge}&apikey=Zl0clXuAbx`)
@@ -25,7 +25,7 @@ import axios from 'axios'
             try {
                 M.reply(
                     buffer || '✖ An error occurred. Please try again later',
-                    MessageType.image,
+                    'image',
                     undefined,
                     undefined,
                     `${data.result.massage}\n *(1)* ${data.result.data[0].title}\n ${data.result.data[0].url}\n\n*(2)* ${data.result.data[1].title}\n ${data.result.data[1].url}\n\n*(3)* ${data.result.data[2].title}\n ${data.result.data[2].url} `,
